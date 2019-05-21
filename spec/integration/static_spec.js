83233b5934ba5239914377653c87f48da500cf70
laynet <layneingramtaylor@gmail.com>
2019-05-21 13:55:53 -0500

    assignment introduction to express complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,12 +6,20 @@ describe("routes : static", () => {
   //#1
   describe("GET /", () => {
     //#2
-    it("should return status code 200", done => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body", done => {
       //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
         //#4
+        done();
+      });
+    });
+  });
+  describe("/marco", () => {
+    it("should return status code 200 and have 'polo' in the body", done => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
         done();
       });
     });
