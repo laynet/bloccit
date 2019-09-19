d85f528d4a24b40d5a33c3ed6bb8e82a0cbd7b7d
Layne <layneingramtaylor@gmail.com>
2019-09-19 13:01:10 -0500

    added advertisement migration

--- src/db/models/advertisement.js
+++ src/db/models/advertisement.js
@@ -1,13 +1,9 @@
-"use strict";
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Advertisement = sequelize.define(
-    "Advertisement",
-    {
-      title: DataTypes.STRING,
-      description: DataTypes.STRING
-    },
-    {}
-  );
+  var Advertisement = sequelize.define('Advertisement', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
   Advertisement.associate = function(models) {
     // associations can be defined here
   };
