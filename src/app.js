36bf86f7cd0d702f9ba4f0f1ffc1b2b2828cf0c6
Layne <layneingramtaylor@gmail.com>
2019-09-20 20:03:57 -0500

    added app.use

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+app.use(function(err, req, res, next) {
+  console.error(err.message); // Log error message in our server's console
+  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+});
+
 appConfig.init(app, express);
 routeConfig.init(app);
 module.exports = app;
