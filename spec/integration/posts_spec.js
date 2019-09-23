f7d2c7c25ca312f5f8b0f201e4c995a6ddcf606f
Layne <layneingramtaylor@gmail.com>
2019-09-23 12:50:16 -0500

    checkpoint validations complete

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -72,6 +72,29 @@ describe("routes : posts", () => {
           });
       });
     });
+    it("should not create a new post that fails validations", done => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          //#1
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+        //#2
+        Post.findOne({ where: { title: "a" } })
+          .then(post => {
+            expect(post).toBeNull();
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+      });
+    });
   });
   describe("GET /topics/:topicId/posts/:id", () => {
     it("should render a view with the selected post", done => {
@@ -137,7 +160,8 @@ describe("routes : posts", () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "I really enjoy the funny hats on them."
         }
       };
       request.post(options, (err, res, body) => {
