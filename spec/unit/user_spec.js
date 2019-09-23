2f6a1eeb8aede205c052e5dc72bb5914c0cb827c
Layne <layneingramtaylor@gmail.com>
2019-09-23 16:07:16 -0500

    checkpoint-authentication complete

--- spec/unit/user_spec.js
+++ spec/unit/user_spec.js
@@ -86,13 +86,4 @@ describe("User", () => {
         });
     });
   });
-  describe("GET /users/sign_in", () => {
-    it("should render a view with a sign in form", done => {
-      request.get(`${base}sign_in`, (err, res, body) => {
-        expect(err).toBeNull();
-        expect(body).toContain("Sign in");
-        done();
-      });
-    });
-  });
 });
