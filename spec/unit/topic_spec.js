5e9966a7cf08e6c099d8b1a837f5b9790e3429ea
Layne <layneingramtaylor@gmail.com>
2019-09-23 20:44:48 -0500

    test failingin topic_spec

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,9 +1,9 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
-const User = require("../../src/db/models").User;
+const User = require("..//../src/db/models").User;
 
-describe("Topic", () => {
+describe("Post", () => {
   beforeEach(done => {
     this.topic;
     this.post;
@@ -21,6 +21,7 @@ describe("Topic", () => {
             title: "Expeditions to Alpha Centauri",
             description:
               "A compilation of reports from recent visits to the star system.",
+
             posts: [
               {
                 title: "My first visit to Proxima Centauri b",
@@ -40,6 +41,53 @@ describe("Topic", () => {
           this.post = topic.posts[0]; //store the post
           done();
         });
+      });
+    });
+  });
+  describe("#create()", () => {
+    it("should create a topic object with a title and description", done => {
+      //#1
+      Topic.create({
+        title:
+          "The best art-house vampire movie is A Girl Walks Home Alone at Night",
+        description:
+          "It's in black and white and Persian so you'll need subtitles if you don't speak Persian"
+      }).then(topic => {
+        //#2
+        expect(topic.title).toBe(
+          "The best art-house vampire movie is A Girl Walks Home Alone at Night"
+        );
+        expect(topic.description).toBe(
+          "It's in black and white and Persian so you'll need subtitles if you don't speak Persian"
+        );
+        done();
+      });
+    });
+    it("should not create a tpoic with missing title or description", done => {
+      Topic.create({
+        title:
+          "The best art-house vampire movie is A Girl Walks Home Alone at Night",
+        description:
+          "It's in black and white and Persian so you'll need subtitles if you don't speak Persian"
+      })
+        .then(topic => {
+          done();
+        })
+        .catch(err => {
+          expect(err.message).toContain("Topic.title cannot be null");
+          expect(err.message).toContain("Topic.description cannot be null");
+          done();
+        });
+    });
+  });
+
+  describe("#getPosts()", () => {
+    it("should return the associated posts", done => {
+      this.topic.getPosts().then(associatedPosts => {
+        expect(associatedPosts[0].title).toBe(
+          "The funniest vampire movie is What We Do In The Shadows"
+        );
+        done();
       });
     });
   });
