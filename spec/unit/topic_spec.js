1b19428d93e3618f83f378111c9d51c0d7ad27e9
Layne <layneingramtaylor@gmail.com>
2019-09-24 21:52:12 -0500

    hasmEny error

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,97 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
+
+describe("Topic", () => {
+  beforeEach(done => {
+    this.topic;
+    this.post;
+    this.user;
+
+    sequelize.sync({ force: true }).then(res => {
+      // #2
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      }).then(user => {
+        this.user = user; //store the user
+
+        // #3
+        Topic.create(
+          {
+            title: "Expeditions to Alpha Centauri",
+            description:
+              "A compilation of reports from recent visits to the star system.",
+
+            // #4
+            posts: [
+              {
+                title: "My first visit to Proxima Centauri b",
+                body: "I saw some rocks.",
+                userId: this.user.id
+              }
+            ]
+          },
+          {
+            // #5
+            include: {
+              model: Post,
+              as: "posts"
+            }
+          }
+        ).then(topic => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
+          done();
+        });
+      });
+    });
+  });
+
+  describe("#create()", () => {
+    it("should create a topic object with a title, and description", done => {
+      Topic.create({
+        title: "Some cool topic title",
+        description: "Some cool topic description."
+      })
+        .then(topic => {
+          expect(topic.title).toBe("Some cool topic title");
+          expect(topic.description).toBe("Some cool topic description.");
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    });
+
+    it("should not create a topic with invalid attributes", done => {
+      Topic.create({
+        title: "A less cool topic title"
+      })
+        .then(topic => {
+          // the code in this block will not be evaluated since the validation error
+          // will skip it. Instead, we'll catch the error in the catch block below
+          // and set the expectations there
+
+          done();
+        })
+        .catch(err => {
+          expect(err.message).toContain("Topic.description cannot be null");
+          done();
+        });
+    });
+  });
+
+  describe("#getPosts()", () => {
+    it("should return the associated posts", done => {
+      this.topic.getPosts().then(associatedPosts => {
+        expect(associatedPosts[0].title).toBe(
+          "My first visit to Proxima Centauri b"
+        );
+        done();
+      });
+    });
+  });
+});
