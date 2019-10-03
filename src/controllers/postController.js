bf1fb469045ec1fde4458404a50a838e9fecf4a2
Layne <layneingramtaylor@gmail.com>
2019-10-03 16:54:19 -0500

    wrote post controller and post policy

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,23 +1,30 @@
 const postQueries = require("../db/queries.posts.js");
+const Authorizer = require("../policies/post");
 
 module.exports = {
   new(req, res, next) {
     res.render("posts/new", { topicId: req.params.topicId });
   },
   create(req, res, next) {
-    let newPost = {
-      title: req.body.title,
-      body: req.body.body,
-      topicId: req.params.topicId,
-      userId: req.user.id
-    };
-    postQueries.addPost(newPost, (err, post) => {
-      if (err) {
-        res.redirect(500, "/posts/new");
-      } else {
-        res.redirect(303, `/topics/${newPost.topicId}/posts/${post.id}`);
-      }
-    });
+    const authorized = new Authorizer(req.user).create();
+    if (authorized) {
+      let newPost = {
+        title: req.body.title,
+        body: req.body.body,
+        topicId: req.params.topicId,
+        userId: req.user.id
+      };
+      postQueries.addPost(newPost, (err, post) => {
+        if (err) {
+          res.redirect(500, "/posts/new");
+        } else {
+          res.redirect(303, `/topics/${newPost.topicId}/posts/${post.id}`);
+        }
+      });
+    } else {
+      req.flash("notice", "You are not authorized to do that.");
+      res.redirect("/posts");
+    }
   },
   show(req, res, next) {
     postQueries.getPost(req.params.id, (err, post) => {
